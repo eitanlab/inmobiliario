178a6aa6c93ac25bfb9c9bcb415a0706eaa3f216
Eitan Labendz <you@example.com>
2020-07-17 01:46:58 -0300

    logic moved to container

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,140 +1,81 @@
 import React from "react";
-import moment from 'moment';
 import { Grid, Paper, Typography, Button, Box, Link } from "@material-ui/core";
-import {CustomPaper} from '../UI/CustomPaper';
+import { CustomPaper } from "../UI/CustomPaper";
 import { useStyles } from "./Post.styles";
 
 const Post = (props) => {
+  const {
+    plan,
+    picture,
+    price,
+    expenses,
+    title,
+    slug,
+    location,
+    description,
+    daysPublished,
+  } = props;
+  const classes = useStyles();
+  const preventDefault = (event) => event.preventDefault();
 
-	let price = null;
-	if (props.posting_prices[0].price) {
-		price = (
-			<Typography variant="h5">
-				{props.posting_prices[0].price.amount.toLocaleString('es-AR', {
-					style: 'currency',
-					currency: props.posting_prices[0].price.currency,
-					minimumFractionDigits: 0,
-				})}
-			</Typography>
-		);
-	}
+  return (
+    <div className={classes.root}>
+      <CustomPaper color={plan.color} className={classes.paper} elevation={2}>
+        <Grid container spacing={0}>
+          <Grid item>
+            <Box className={classes.image}>
+              <img className={classes.img} alt="complex" src={picture} />
+              <Typography className={classes.imgCaption} variant="caption">
+                {plan.type}
+              </Typography>
+            </Box>
+            <Box marginBottom={1} className={classes.amounts}>
+              <Typography variant="h5">{price}</Typography>
+              <Typography variant="subtitle2">+ {expenses} Expensas</Typography>
+            </Box>
+          </Grid>
+          <Grid className={classes.rightColumn} item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Box marginBottom={4}>
+                  <Typography gutterBottom variant="subtitle1">
+                    <Link
+                      href={slug}
+                      onClick={preventDefault}
+                      color="secondary"
+                    >
+                      {title}
+                    </Link>
+                  </Typography>
+                  <Typography variant="body2" gutterBottom>
+                    {location}
+                  </Typography>
+                </Box>
 
-	let expenses = null;
-	if (props.posting_prices[0].expenses) {
-		expenses = (
-			<Typography variant="subtitle2">
-			{`+ 
-			${props.posting_prices[0].expenses.amount.toLocaleString('es-AR', {
-				style: 'currency',
-				currency: props.posting_prices[0].expenses.currency,
-				minimumFractionDigits: 0,
-			})}
-			Expensas`}
-			</Typography>
-		);
-	}
-
-	let location = null;
-	if (props.posting_location) {
-		location = `${props.posting_location.address}, ${props.posting_location.zone}, ${props.posting_location.city}`
-	}
-
-	let description = null;
-	if (props.posting_description) {
-		description = props.posting_description.substring(0, 320) + '...';
-	}
-
-	const publicationPlan = () => {
-		switch(props.publication_plan) {
-			case 'SIMPLE':
-				return {
-					type: 'Simple',
-					color: ''
-				};
-			case 'HIGHLIGHTED':
-				return {
-					type: 'Destacado',
-					color: '#31d1a1'
-				};
-			case 'SUPERHIGHLIGHTED':
-				return {
-					type: 'Súper destacado',
-					color: '#9371e0'
-				};
-			default:
-				return null;
-		} 
-	}
-
-	const classes = useStyles();
-
-	const preventDefault = (event) => event.preventDefault();
-
-	let publishedDaysAgo = null
-	if (props.publish_date) {
-		const publishedDate = moment(props.publish_date, 'DD/MM/YYYY').format();
-		const timeDiff  = (new Date()) - (new Date(publishedDate));
-		publishedDaysAgo = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-	}
-
-	return (
-		<div className={classes.root}>
-		<CustomPaper color={publicationPlan().color} className={classes.paper} elevation={2}>
-			<Grid container spacing={0}>
-			<Grid item>
-				<Box className={classes.image}>
-				<img
-					className={classes.img}
-					alt="complex"
-					src={props.posting_picture}
-				/>
-				<Typography className={classes.imgCaption} variant="caption">
-					{publicationPlan().type}
-				</Typography>
-				</Box>
-				<Box marginBottom={1} className={classes.amounts}>
-					{price}
-					{expenses}
-				</Box>
-			</Grid>
-			<Grid className={classes.rightColumn} item xs={12} sm container>
-				<Grid item xs container direction="column" spacing={2}>
-				<Grid item xs>
-					<Box marginBottom={4}>
-						<Typography gutterBottom variant="subtitle1">
-							<Link href={props.posting_slug} onClick={preventDefault} color='secondary'>
-								{props.title}
-							</Link>
-						</Typography>
-						<Typography variant="body2" gutterBottom>
-							{location}
-						</Typography>
-					</Box>
-					
-					<Typography variant="body2" color="textSecondary">
-						{description}
-					</Typography>
-				</Grid>
-				<Grid item>
-					<Box
-					display="flex"
-					justifyContent="space-between"
-					alignItems="center"
-					>
-					<Typography variant="body2" style={{ cursor: "pointer" }}>
-						Publicado hace {publishedDaysAgo} días
-					</Typography>
-					<Button variant="contained" color="primary">
-						Contactar
-					</Button>
-					</Box>
-				</Grid>
-				</Grid>
-			</Grid>
-			</Grid>
-		</CustomPaper>
-		</div>
-	);
+                <Typography variant="body2" color="textSecondary">
+                  {description.substring(0, 320)}...
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Box
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Typography variant="body2">
+                    Publicado hace {daysPublished} días
+                  </Typography>
+                  <Button variant="contained" color="primary">
+                    Contactar
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CustomPaper>
+    </div>
+  );
 };
 
 export default Post;
--- src/containers/SearchResults/SearchResults.jsx
+++ src/containers/SearchResults/SearchResults.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import moment from 'moment';
 import classes from "./SearchResults.module.scss";
 import { mockedPostings } from '../../___mock__/mockedPostings';
 import Sidebar from "../../components/Sidebar/Sidebar";
@@ -10,17 +11,81 @@ const SearchResults = (props) => {
   const [postings, setPostings] = useState([]);
 
   useEffect(() => {
-    
       const getPostings = async () => {
         try {
-          setPostings(mockedPostings);
-          console.log(postings);
+          mockedPostings.map(post => {
+            const curatedPost = {};
+            curatedPost['price'] = setPostPrices(post.posting_prices[0].price);
+            curatedPost['expenses'] = setPostPrices(post.posting_prices[0].expenses);
+            curatedPost['location'] = setPostLocation(post.posting_location);
+            curatedPost['title'] = post.title;
+            curatedPost['slug'] = post.posting_slug;
+            curatedPost['description'] = post.posting_description;
+            curatedPost['plan'] = setPostPublicationPlan(post.publication_plan);
+            curatedPost['daysPublished'] = setPostDaysPublished(post.publish_date);
+            curatedPost['picture'] = post.posting_picture;
+            console.log(curatedPost)
+            setPostings(postings => [...postings,curatedPost]);
+          });
+          //console.log(postings);
         } catch (error) {
-          alert(`Postings can't be loaded because (${error})`);
+          alert(`Postings can't be loaded because: ${error}`);
         }
       };
       getPostings();
   }, []);
+
+  const setPostPrices = value => {
+    if (value) {
+      return value.amount.toLocaleString('es-AR', {
+          style: 'currency',
+          currency: value.currency,
+          minimumFractionDigits: 0,
+      });
+    }
+  }
+
+  const setPostLocation = location => {
+    if (location) {
+      return `${location.address}, ${location.zone}, ${location.city}`
+    }
+  }
+
+	const setPostShortDescription = description => {
+    if (description) {
+      return description.substring(0, 320) + '...';
+    }
+  }
+
+  const setPostPublicationPlan = plan => {
+		switch(plan) {
+			case 'SIMPLE':
+				return {
+					type: 'Simple',
+					color: ''
+				};
+			case 'HIGHLIGHTED':
+				return {
+					type: 'Destacado',
+					color: '#31d1a1'
+				};
+			case 'SUPERHIGHLIGHTED':
+				return {
+					type: 'Súper destacado',
+					color: '#9371e0'
+				};
+			default:
+				return null;
+		} 
+  }
+
+  const setPostDaysPublished = date => {
+    if (date) {
+      const publishedDate = moment(date, 'DD/MM/YYYY').format();
+      const timeDiff  = (new Date()) - (new Date(publishedDate));
+      return Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    }
+  } 
 
   return (
     <Container>
